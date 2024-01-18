67afea74b8caeb781a80aa8d9c4d50428bb16baf
Calebbii <biicaleb316@gmail.com>
2024-01-18 04:28:35 +0300

    routing

--- server/app.py
+++ server/app.py
@@ -6,3 +6,11 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    return f'<h1>{parameter}</h1>'
